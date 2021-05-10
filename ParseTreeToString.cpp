88a58e4b03caab70454dc87907532d3c9f822d80
Shrividya Ballapadavu <shrividya94@gmail.com>
2021-05-10 14:15:31 -0700

    updated parseTreeToString

--- ParseTreeToString.cpp
+++ ParseTreeToString.cpp
@@ -8,53 +8,52 @@
 using namespace std;
 using namespace hsql;
 
-const vector <string> ParseTreeToString::reserved_words = {"COLUMNS", "SHOW", "TABLES", "ADD", "ALL", "ALLOCATE",
-                                                           "ALTER", "AND", "ANY", "ARE", "ARRAY", "AS", "ASENSITIVE",
-                                                           "ASYMMETRIC", "AT", "ATOMIC", "AUTHORIZATION", "BEGIN",
-                                                           "BETWEEN", "BIGINT", "BINARY", "BLOB", "BOOLEAN", "BOTH",
-                                                           "BY", "CALL", "CALLED", "CASCADED", "CASE", "CAST", "CHAR",
-                                                           "CHARACTER", "CHECK", "CLOB", "CLOSE", "COLLATE", "COLUMN",
-                                                           "COMMIT", "CONNECT", "CONSTRAINT", "CONTINUE",
-                                                           "CORRESPONDING", "CREATE", "CROSS", "CUBE", "CURRENT",
-                                                           "CURRENT_DATE", "CURRENT_DEFAULT_TRANSFORM_GROUP",
-                                                           "CURRENT_PATH", "CURRENT_ROLE", "CURRENT_TIME",
-                                                           "CURRENT_TIMESTAMP", "CURRENT_TRANSFORM_GROUP_FOR_TYPE",
-                                                           "CURRENT_USER", "CURSOR", "CYCLE", "DATE", "DAY",
-                                                           "DEALLOCATE", "DEC", "DECIMAL", "DECLARE", "DEFAULT",
-                                                           "DELETE", "DEREF", "DESCRIBE", "DETERMINISTIC", "DISCONNECT",
-                                                           "DISTINCT", "DOUBLE", "DROP", "DYNAMIC", "EACH", "ELEMENT",
-                                                           "ELSE", "END", "END-EXEC", "ESCAPE", "EXCEPT", "EXEC",
-                                                           "EXECUTE", "EXISTS", "EXTERNAL", "FALSE", "FETCH", "FILTER",
-                                                           "FLOAT", "FOR", "FOREIGN", "FREE", "FROM", "FULL",
-                                                           "FUNCTION", "GET", "GLOBAL", "GRANT", "GROUP", "GROUPING",
-                                                           "HAVING", "HOLD", "HOUR", "IDENTITY", "IMMEDIATE", "IN",
-                                                           "INDICATOR", "INNER", "INOUT", "INPUT", "INSENSITIVE",
-                                                           "INSERT", "INT", "INTEGER", "INTERSECT", "INTERVAL", "INTO",
-                                                           "IS", "ISOLATION", "JOIN", "LANGUAGE", "LARGE", "LATERAL",
-                                                           "LEADING", "LEFT", "LIKE", "LOCAL", "LOCALTIME",
-                                                           "LOCALTIMESTAMP", "MATCH", "MEMBER", "MERGE", "METHOD",
-                                                           "MINUTE", "MODIFIES", "MODULE", "MONTH", "MULTISET",
-                                                           "NATIONAL", "NATURAL", "NCHAR", "NCLOB", "NEW", "NO", "NONE",
-                                                           "NOT", "NULL", "NUMERIC", "OF", "OLD", "ON", "ONLY", "OPEN",
-                                                           "OR", "ORDER", "OUT", "OUTER", "OUTPUT", "OVER", "OVERLAPS",
-                                                           "PARAMETER", "PARTITION", "PRECISION", "PREPARE", "PRIMARY",
-                                                           "PROCEDURE", "RANGE", "READS", "REAL", "RECURSIVE", "REF",
-                                                           "REFERENCES", "REFERENCING", "REGR_AVGX", "REGR_AVGY",
-                                                           "REGR_COUNT", "REGR_INTERCEPT", "REGR_R2", "REGR_SLOPE",
-                                                           "REGR_SXX", "REGR_SXY", "REGR_SYY", "RELEASE", "RESULT",
-                                                           "RETURN", "RETURNS", "REVOKE", "RIGHT", "ROLLBACK", "ROLLUP",
-                                                           "ROW", "ROWS", "SAVEPOINT", "SCROLL", "SEARCH", "SECOND",
-                                                           "SELECT", "SENSITIVE", "SESSION_USER", "SET", "SIMILAR",
-                                                           "SMALLINT", "SOME", "SPECIFIC", "SPECIFICTYPE", "SQL",
-                                                           "SQLEXCEPTION", "SQLSTATE", "SQLWARNING", "START", "STATIC",
-                                                           "SUBMULTISET", "SYMMETRIC", "SYSTEM", "SYSTEM_USER", "TABLE",
-                                                           "THEN", "TIME", "TIMESTAMP", "TIMEZONE_HOUR",
-                                                           "TIMEZONE_MINUTE", "TO", "TRAILING", "TRANSLATION", "TREAT",
-                                                           "TRIGGER", "TRUE", "UESCAPE", "UNION", "UNIQUE", "UNKNOWN",
-                                                           "UNNEST", "UPDATE", "UPPER", "USER", "USING", "VALUE",
-                                                           "VALUES", "VAR_POP", "VAR_SAMP", "VARCHAR", "VARYING",
-                                                           "WHEN", "WHENEVER", "WHERE", "WIDTH_BUCKET", "WINDOW",
-                                                           "WITH", "WITHIN", "WITHOUT", "YEAR"};
+const vector<string> ParseTreeToString::reserved_words = {"COLUMNS", "SHOW", "TABLES", "ADD", "ALL", "ALLOCATE",
+                                                          "ALTER", "AND", "ANY", "ARE", "ARRAY", "AS", "ASENSITIVE",
+                                                          "ASYMMETRIC", "AT", "ATOMIC", "AUTHORIZATION", "BEGIN",
+                                                          "BETWEEN", "BIGINT", "BINARY", "BLOB", "BOOLEAN", "BOTH",
+                                                          "BY", "CALL", "CALLED", "CASCADED", "CASE", "CAST", "CHAR",
+                                                          "CHARACTER", "CHECK", "CLOB", "CLOSE", "COLLATE", "COLUMN",
+                                                          "COMMIT", "CONNECT", "CONSTRAINT", "CONTINUE",
+                                                          "CORRESPONDING", "CREATE", "CROSS", "CUBE", "CURRENT",
+                                                          "CURRENT_DATE", "CURRENT_DEFAULT_TRANSFORM_GROUP",
+                                                          "CURRENT_PATH", "CURRENT_ROLE", "CURRENT_TIME",
+                                                          "CURRENT_TIMESTAMP", "CURRENT_TRANSFORM_GROUP_FOR_TYPE",
+                                                          "CURRENT_USER", "CURSOR", "CYCLE", "DATE", "DAY",
+                                                          "DEALLOCATE", "DEC", "DECIMAL", "DECLARE", "DEFAULT",
+                                                          "DELETE", "DEREF", "DESCRIBE", "DETERMINISTIC", "DISCONNECT",
+                                                          "DISTINCT", "DOUBLE", "DROP", "DYNAMIC", "EACH", "ELEMENT",
+                                                          "ELSE", "END", "END-EXEC", "ESCAPE", "EXCEPT", "EXEC",
+                                                          "EXECUTE", "EXISTS", "EXTERNAL", "FALSE", "FETCH", "FILTER",
+                                                          "FLOAT", "FOR", "FOREIGN", "FREE", "FROM", "FULL", "FUNCTION",
+                                                          "GET", "GLOBAL", "GRANT", "GROUP", "GROUPING", "HAVING",
+                                                          "HOLD", "HOUR", "IDENTITY", "IMMEDIATE", "IN", "INDICATOR",
+                                                          "INNER", "INOUT", "INPUT", "INSENSITIVE", "INSERT", "INT",
+                                                          "INTEGER", "INTERSECT", "INTERVAL", "INTO", "IS", "ISOLATION",
+                                                          "JOIN", "LANGUAGE", "LARGE", "LATERAL", "LEADING", "LEFT",
+                                                          "LIKE", "LOCAL", "LOCALTIME", "LOCALTIMESTAMP", "MATCH",
+                                                          "MEMBER", "MERGE", "METHOD", "MINUTE", "MODIFIES", "MODULE",
+                                                          "MONTH", "MULTISET", "NATIONAL", "NATURAL", "NCHAR", "NCLOB",
+                                                          "NEW", "NO", "NONE", "NOT", "NULL", "NUMERIC", "OF", "OLD",
+                                                          "ON", "ONLY", "OPEN", "OR", "ORDER", "OUT", "OUTER", "OUTPUT",
+                                                          "OVER", "OVERLAPS", "PARAMETER", "PARTITION", "PRECISION",
+                                                          "PREPARE", "PRIMARY", "PROCEDURE", "RANGE", "READS", "REAL",
+                                                          "RECURSIVE", "REF", "REFERENCES", "REFERENCING", "REGR_AVGX",
+                                                          "REGR_AVGY", "REGR_COUNT", "REGR_INTERCEPT", "REGR_R2",
+                                                          "REGR_SLOPE", "REGR_SXX", "REGR_SXY", "REGR_SYY", "RELEASE",
+                                                          "RESULT", "RETURN", "RETURNS", "REVOKE", "RIGHT", "ROLLBACK",
+                                                          "ROLLUP", "ROW", "ROWS", "SAVEPOINT", "SCROLL", "SEARCH",
+                                                          "SECOND", "SELECT", "SENSITIVE", "SESSION_USER", "SET",
+                                                          "SIMILAR", "SMALLINT", "SOME", "SPECIFIC", "SPECIFICTYPE",
+                                                          "SQL", "SQLEXCEPTION", "SQLSTATE", "SQLWARNING", "START",
+                                                          "STATIC", "SUBMULTISET", "SYMMETRIC", "SYSTEM", "SYSTEM_USER",
+                                                          "TABLE", "THEN", "TIME", "TIMESTAMP", "TIMEZONE_HOUR",
+                                                          "TIMEZONE_MINUTE", "TO", "TRAILING", "TRANSLATION", "TREAT",
+                                                          "TRIGGER", "TRUE", "UESCAPE", "UNION", "UNIQUE", "UNKNOWN",
+                                                          "UNNEST", "UPDATE", "UPPER", "USER", "USING", "VALUE",
+                                                          "VALUES", "VAR_POP", "VAR_SAMP", "VARCHAR", "VARYING", "WHEN",
+                                                          "WHENEVER", "WHERE", "WIDTH_BUCKET", "WINDOW", "WITH",
+                                                          "WITHIN", "WITHOUT", "YEAR"};
 
 bool ParseTreeToString::is_reserved_word(string candidate) {
     for (auto const &word: reserved_words)
@@ -108,7 +107,6 @@ string ParseTreeToString::expression(const Expr *expr) {
         case kExprLiteralInt:
             ret += to_string(expr->ival);
             break;
-
         case kExprFunctionRef:
             ret += string(expr->name) + "?" + expr->expr->name;
             break;
@@ -212,29 +210,34 @@ string ParseTreeToString::insert(const InsertStatement *stmt) {
 
 string ParseTreeToString::create(const CreateStatement *stmt) {
     string ret("CREATE ");
-    if (stmt->type == CreateStatement::kIndex) {
+    if (stmt->type == CreateStatement::kTable) {
+        ret += "TABLE ";
+        if (stmt->ifNotExists)
+            ret += "IF NOT EXISTS ";
+        ret += string(stmt->tableName) + " (";
+        bool doComma = false;
+        for (ColumnDefinition *col : *stmt->columns) {
+            if (doComma)
+                ret += ", ";
+            ret += column_definition(col);
+            doComma = true;
+        }
+        ret += ")";
+    } else if (stmt->type == CreateStatement::kIndex) {
         ret += "INDEX ";
-        ret += stmt->indexName;
-        ret += " ON ";
-        ret += stmt->tableName;
-        ret += " USING BTREE ";
-        return ret;
+        ret += string(stmt->indexName) + " ON ";
+        ret += string(stmt->tableName) + " USING " + stmt->indexType + " (";
+        bool doComma = false;
+        for (auto const &col : *stmt->indexColumns) {
+            if (doComma)
+                ret += ", ";
+            ret += string(col);
+            doComma = true;
+        }
+        ret += ")";
+    } else {
+        ret += "...";
     }
-
-    if (stmt->type != CreateStatement::kTable)
-        return ret + "...";
-    ret += "TABLE ";
-    if (stmt->ifNotExists)
-        ret += "IF NOT EXISTS ";
-    ret += string(stmt->tableName) + " (";
-    bool doComma = false;
-    for (ColumnDefinition *col : *stmt->columns) {
-        if (doComma)
-            ret += ", ";
-        ret += column_definition(col);
-        doComma = true;
-    }
-    ret += ")";
     return ret;
 }
 
@@ -245,9 +248,8 @@ string ParseTreeToString::drop(const DropStatement *stmt) {
             ret += "TABLE ";
             break;
         case DropStatement::kIndex:
-            ret += "INDEX ";
-            ret +=stmt->indexName;
-            ret +=" FROM ";
+            ret += "INDEX " + string(stmt->indexName);
+            ret += " FROM ";
             break;
         default:
             ret += "? ";
@@ -266,8 +268,7 @@ string ParseTreeToString::show(const ShowStatement *stmt) {
             ret += string("COLUMNS FROM ") + stmt->tableName;
             break;
         case ShowStatement::kIndex:
-            ret += "INDEX FROM ";
-            ret += stmt->tableName;
+            ret += string("INDEX FROM ") + stmt->tableName;
             break;
         default:
             ret += "?what?";
